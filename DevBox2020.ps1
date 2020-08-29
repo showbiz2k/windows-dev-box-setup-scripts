5dc9c0631a7b37c4c8c57bdd937a69e7d0db7e13
showbiz2k <matthew.jackson@outlook.com>
2020-08-29 19:37:22 -0400

    Correcting execution of support scripts

--- DevBox2020.ps1
+++ DevBox2020.ps1
@@ -2,14 +2,20 @@ Disable-UAC
 $ConfirmPreference = "None" #ensure installing powershell modules don't prompt on needed dependencies
 
 # Get the base URI path from the ScriptToCall value
-
-$helperScriptLocation = Get-PackageRoot($MyInvocation)
-write-host "helper script base URI is $helperScriptLocation"
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
 
 function executeScript {
     Param ([string]$script)
-    write-host "executing $helperScriptLocation\scripts\$script ..."
-    & "$helperScriptLocation\scripts\$script"
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
 }
 
 #--- Setting up Windows ---
