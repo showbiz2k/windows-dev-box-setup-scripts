cb8d1aac6c3f30223ae2379a8aea1c948b237777
showbiz2k <matthew.jackson@outlook.com>
2020-08-29 20:32:10 -0400

    update hyper v installation

--- scripts/HyperV.ps1
+++ scripts/HyperV.ps1
@@ -1,1 +1,6 @@
-choco install -y Microsoft-Hyper-V-All --source="'windowsFeatures'"
+Write-Output "Installing Hyper-V..."
+If ((Get-WmiObject -Class "Win32_OperatingSystem").Caption -like "*Server*") {
+    Install-WindowsFeature -Name "Hyper-V" -IncludeManagementTools -WarningAction SilentlyContinue | Out-Null
+} Else {
+    Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -NoRestart -WarningAction SilentlyContinue | Out-Null
+}
