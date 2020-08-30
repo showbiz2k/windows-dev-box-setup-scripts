ce4d88c83a54edda985ea201e0ac15a004b17579
showbiz2k <matthew.jackson@outlook.com>
2020-08-29 20:41:30 -0400

    fixing dupe function

--- demos/DevBox2020.ps1
+++ demos/DevBox2020.ps1
@@ -19,12 +19,6 @@ function executeScript {
 	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
 }
 
-function executeScript {
-    Param ([string]$script)
-    write-host "executing $helperUri/$script ..."
-	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
-}
-
 #--- Setting up Windows ---
 executeScript "FileExplorerSettings.ps1";
 executeScript "SystemConfiguration.ps1";
